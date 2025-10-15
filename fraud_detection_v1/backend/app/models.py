43e7bc43e675eeb06d8f5c23739abaf97de61872
Tejaswee Sulekh <20d070082@iitb.ac.in>
2025-10-15 22:03:26 +0530

    Added the template class for transaction

---
+++ fraud_detection_v1/backend/app/models.py
@@ -0,0 +1,14 @@
+from sqlalchemy import Boolean, Column, Integer, String, Float, DateTime
+from .database import Base
+import datetime
+
+class Transaction(Base):
+    __tablename__ = "transactions"
+
+    id = Column(Integer, primary_key=True, index=True)
+    amount = Column(Float, index=True)
+    merchant = Column(String)
+    location = Column(String)
+    timestamp = Column(DateTime, default=datetime.datetime.utcnow)
+    is_fraud = Column(Boolean, default=False)
+    fraud_reason = Column(String, nullable=True)
